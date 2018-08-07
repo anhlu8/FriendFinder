4006d96b813391c1a63c834d9def9d6a3c420749
Anh Lu <anhlucci@outlook.com>
2018-08-06 21:54:42 -0400

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = 
+[{
+    "name":"Anh",
+    "photo":"https://goo.gl/images/fFVzyc",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }];
+  console.log(friends);
+
+  module.exports = friends;
+  
