7f22ef1cc3192c2249f025bc7d7b7032dee2cfcf
Anh Lu <anhlucci@outlook.com>
2018-08-10 16:52:53 -0400

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,35 @@
-var friends = 
-[{
-    "name":"Anh",
-    "photo":"https://goo.gl/images/fFVzyc",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }];
-  console.log(friends);
+var friends = [{
+        "name": "Anh",
+        "photo": "https://goo.gl/images/fFVzyc",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Thanh",
+        "photo": "https://goo.gl/images/fFVzyc",
+        "scores": [
+            4,
+            5,
+            3,
+            5,
+            2,
+            5,
+            1,
+            4,
+            3,
+            5
+        ]
+    },
+];
 
-  module.exports = friends;
-  
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,44 @@
 var friendData = require("../data/friends");
 
 
-module.exports = function(app){
-app.get("/api/friends", function(req, res){
-    res.json(friendData);
-});
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
 
-app.post("/api/friends", function(req, res){
-    var newFriend = req.body;
-    friendData.push(newFriend);
-    console.log(newFriend);
-});
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+        var newFriendScore = newFriend.scores.map(function (i) {
+            return parseInt(i);
+        });
+        
+        var bestFriend;
+        var bestFriendTotal = 1000;
+
+
+        for (var i = 0; i < friendData.length; i++){
+            var currentFriendScore = friendData[i].scores;
+
+            var sumOfDiff = 0;
+           
+            for (var j = 0; j<currentFriendScore.length; j++){
+                var diff = newFriendScore[j] - parseInt(currentFriendScore[j]);
+                sumOfDiff += Math.abs(diff);
+             
+            }
+            console.log(sumOfDiff)
+            console.log(bestFriendTotal)
+            if (sumOfDiff < bestFriendTotal) {
+                bestFriendTotal = sumOfDiff;
+                bestFriend = friendData[i];
+            }
+
+        }
+
+        console.log("friendData: " , friendData);
+        console.log("newfriendscores: " , newFriendScore);
+        // console.log("newfriend: " , newFriend);
+        friendData.push(newFriend);
+        res.json(bestFriend);
+    });
 };
